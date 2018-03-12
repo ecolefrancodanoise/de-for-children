205feb862ad48c0af0c2502b1f669f9071647815
Sacha Guilbert <sachaguilbert@gmail.com>
2018-03-12 15:39:37 +0100

    Fix kill-prey button

--- cpp/predator-prey.cpp
+++ cpp/predator-prey.cpp
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+
 int main(){
+    int executionTime = time(NULL);
     double predator = 1e2; //Number of Predators
     double prey = 1e4; //Number of Preys
     double alpha1 = 5e-2; //Number of new preys per day (per prey)
@@ -8,12 +12,15 @@ int main(){
     double dt = 0.1; //Time step is one day
     double t = 0;
     
-    for (int i = 0; i<1e4; i = i+1) {
+    for (int i = 0; i<1e5; i = i+1) {
         t = t + dt;
         double dPrey = dt*(alpha1 * prey - beta * prey * predator);
         double dPredator = dt*(delta * prey * predator - gamma * predator);
         prey = prey + dPrey;
         predator = predator + dPredator;
     }
+    int executionTime2 = time(NULL);
+    printf("%d\n", executionTime);
+    printf("%d\n", executionTime2);
 
 }
